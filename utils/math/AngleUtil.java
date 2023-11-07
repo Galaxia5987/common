f0ab7d5167277912f40e7d589bd30a59cb5a1d4a
eitan <eitan.vichik@gmail.com>
2023-11-07 18:04:24 +0200

    Fix bug in normalize function

--- utils/math/AngleUtil.java
+++ utils/math/AngleUtil.java
@@ -32,7 +32,7 @@ public class AngleUtil {
         while (angle < 0) {
             angle += 2 * Math.PI;
         }
-        return angle % 2 * Math.PI;
+        return angle % (2 * Math.PI);
     }
 
     public static double absoluteAngleToYaw(double angle) {
