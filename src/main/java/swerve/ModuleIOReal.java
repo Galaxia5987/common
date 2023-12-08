875654ee0794348214753316e155024f7e8c7a0f
Emma <emmalivne.03@gmail.com>
2023-12-08 12:41:43 +0200

    Update ModuleIOReal to phoenix 6

--- src/main/java/swerve/ModuleIOReal.java
+++ src/main/java/swerve/ModuleIOReal.java
@@ -2,22 +2,18 @@ package swerve;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfigurator;
-import com.ctre.phoenix6.controls.MotionMagicDutyCycle;
-import com.ctre.phoenix6.controls.MotionMagicVoltage;
-import com.ctre.phoenix6.controls.VelocityDutyCycle;
+import com.ctre.phoenix6.controls.*;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
-import frc.robot.Constants;
-import frc.robot.Ports;
 import utils.math.AngleUtil;
 import utils.math.differential.Integral;
-import utils.units.UnitModel;
+import utils.units.Phoenix6UnitModel;
+import utils.units.Units;
 
-public class
-ModuleIOReal implements ModuleIO { //TODO: keep updating to phoenix6 and FIX UNITS!!
+public class ModuleIOReal implements ModuleIO {
 
     private final TalonFX driveMotor;
     private final TalonFXConfiguration driveMotorConfig = new TalonFXConfiguration();
@@ -30,8 +26,10 @@ ModuleIOReal implements ModuleIO { //TODO: keep updating to phoenix6 and FIX UNI
     private final DutyCycleEncoder encoder;
 
     private final double[] motionMagicConfigs;
-    private final UnitModel ticksPerRad = new UnitModel(SwerveConstants.TICKS_PER_RADIAN);
-    private final UnitModel ticksPerMeter = new UnitModel(SwerveConstants.TICKS_PER_METER);
+
+    private final Phoenix6UnitModel driveMotorUnitModel = new Phoenix6UnitModel(1/SwerveConstants.DRIVE_REDUCTION);
+    private final Phoenix6UnitModel angleMotorUnitModel = new Phoenix6UnitModel(1/SwerveConstants.ANGLE_REDUCTION);
+
     private final int number;
     private final Integral driveSupplyChargeUsedCoulomb = new Integral();
     private final Integral driveStatorChargeUsedCoulomb = new Integral();
@@ -65,17 +63,21 @@ ModuleIOReal implements ModuleIO { //TODO: keep updating to phoenix6 and FIX UNI
         driveMotorConfig.Slot0.kV = SwerveConstants.DRIVE_KF;
         driveMotorConfig.Voltage.PeakForwardVoltage = SwerveConstants.VOLT_COMP_SATURATION;
         driveMotorConfig.Voltage.PeakReverseVoltage = -SwerveConstants.VOLT_COMP_SATURATION;
-        driveMotor.setNeutralMode(NeutralModeValue.Brake);
         driveMotorConfig.CurrentLimits.SupplyCurrentLimit = SwerveConstants.SUPPLY_CURRENT_LIMIT.currentLimit;
         driveMotorConfig.CurrentLimits.StatorCurrentLimit = SwerveConstants.STATOR_CURRENT_LIMIT.currentLimit;
+        driveMotorConfig.Feedback.SensorToMechanismRatio = 1/SwerveConstants.DRIVE_REDUCTION;
+        driveMotorConfig.Feedback.RotorToSensorRatio = 1/1;
+        driveMotor.setNeutralMode(NeutralModeValue.Brake);
         driveMotor.setInverted(true);
         driveMotorConfigurator.apply(driveMotorConfig);
 
         angleMotorConfig.Voltage.PeakForwardVoltage = SwerveConstants.VOLT_COMP_SATURATION;
         angleMotorConfig.Voltage.PeakReverseVoltage = -SwerveConstants.VOLT_COMP_SATURATION;
-        angleMotor.setNeutralMode(NeutralModeValue.Brake);
         angleMotorConfig.CurrentLimits.SupplyCurrentLimit = SwerveConstants.SUPPLY_CURRENT_LIMIT.currentLimit;
         angleMotorConfig.CurrentLimits.StatorCurrentLimit = SwerveConstants.STATOR_CURRENT_LIMIT.currentLimit;
+        angleMotorConfig.Feedback.SensorToMechanismRatio = 1/SwerveConstants.ANGLE_REDUCTION;
+        angleMotorConfig.Feedback.RotorToSensorRatio = 1/1;
+        angleMotor.setNeutralMode(NeutralModeValue.Brake);
         angleMotor.setInverted(true);
         angleMotorConfig.MotorOutput.DutyCycleNeutralDeadband = SwerveConstants.NEUTRAL_DEADBAND;
 
@@ -112,7 +114,7 @@ ModuleIOReal implements ModuleIO { //TODO: keep updating to phoenix6 and FIX UNI
         inputs.angleMotorStatorCurrentOverTime = angleStatorChargeUsedCoulomb.get();
         inputs.angleMotorPosition = angleMotor.getRotorPosition().getValue();
         angleMotorPosition = inputs.angleMotorPosition;
-        inputs.angleMotorVelocity = ticksPerMeter.toVelocity(angleMotor.getVelocity().getValue());
+        inputs.angleMotorVelocity = angleMotor.getVelocity().getValue();
 
         inputs.angle = getAngle();
         currentAngle = inputs.angle;
@@ -124,20 +126,20 @@ ModuleIOReal implements ModuleIO { //TODO: keep updating to phoenix6 and FIX UNI
 
     @Override
     public double getAngle() {
-        return AngleUtil.normalize(ticksPerRad.toUnits(angleMotor.getRotorPosition().getValue()));
+        return AngleUtil.normalize(Units.rotationsToRad(angleMotor.getPosition().getValue()));
     }
 
     @Override
     public void setAngle(double angle) {
         angleSetpoint = AngleUtil.normalize(angle);
         Rotation2d error = new Rotation2d(angle).minus(new Rotation2d(currentAngle));
-        var motorRequest = new MotionMagicVoltage(angleMotorPosition + ticksPerRad.toTicks(error.getRadians()));
+        var motorRequest = new MotionMagicVoltage(angleMotorPosition + error.getRotations());
         angleMotor.setControl(motorRequest);
     }
 
     @Override
     public double getVelocity() {
-        return ticksPerMeter.toVelocity(driveMotor.getRotorVelocity().getValue());
+        return driveMotor.getVelocity().getValue();
     }
 
     @Override
@@ -145,28 +147,34 @@ ModuleIOReal implements ModuleIO { //TODO: keep updating to phoenix6 and FIX UNI
         var angleError = new Rotation2d(angleSetpoint).minus(new Rotation2d(currentAngle));
         velocity *= angleError.getCos();
         driveMotorVelocitySetpoint = velocity;
-        var motorRequest = new VelocityDutyCycle(ticksPerMeter.toTicks100ms(velocity));
+        var motorRequest = new VelocityDutyCycle(
+                Units.metersPerSecondToRps(
+                        velocity,
+                        SwerveConstants.WHEEL_DIAMETER / 2
+                )
+        );
         driveMotor.setControl(motorRequest);
     }
 
     @Override
     public SwerveModulePosition getModulePosition() {
         return new SwerveModulePosition(
-                ticksPerMeter.toUnits(driveMotor.getSelectedSensorPosition()),
+                Units.rpsToMetersPerSecond(driveMotor.getPosition().getValue(), SwerveConstants.WHEEL_DIAMETER),
                 new Rotation2d(getAngle())
         );
     }
 
     @Override
     public void updateOffset(double offset) {
-        angleMotor.setSelectedSensorPosition(
+        angleMotor.setPosition(
                 ((encoder.getAbsolutePosition() - offset) * 2048) / SwerveConstants.ANGLE_REDUCTION);
     }
 
     @Override
     public void neutralOutput() {
-        driveMotor.neutralOutput();
-        angleMotor.neutralOutput();
+        var motorRequest = new NeutralOut();
+        driveMotor.setControl(motorRequest);
+        angleMotor.setControl(motorRequest);
     }
 
     @Override
@@ -176,7 +184,9 @@ ModuleIOReal implements ModuleIO { //TODO: keep updating to phoenix6 and FIX UNI
 
     @Override
     public void checkModule() {
-        driveMotor.set(TalonFXControlMode.PercentOutput, 0.8);
-        angleMotor.set(TalonFXControlMode.PercentOutput, 0.2);
+        var driveMotorRequest = new DutyCycleOut(0.8);
+        var angleMotorRequest = new DutyCycleOut(0.2);
+        driveMotor.setControl(driveMotorRequest);
+        angleMotor.setControl(angleMotorRequest);
     }
 }
