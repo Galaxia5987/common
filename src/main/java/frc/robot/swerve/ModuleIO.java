da51733d0f0790a0f5d8399cb37cb1cb46456ad2
Emma <emmalivne.03@gmail.com>
2023-12-17 18:05:00 +0200

    Remove auto logged swerveModuleInputs

--- src/main/java/frc/robot/swerve/ModuleIO.java
+++ src/main/java/frc/robot/swerve/ModuleIO.java
@@ -37,29 +37,4 @@ public interface ModuleIO {
     default Command checkModule() {
         return null;
     }
-
-    @AutoLog
-    class SwerveModuleInputs {
-        public double driveMotorVelocity = 0;
-        public double driveMotorVelocitySetpoint = 0;
-        public double driveMotorSupplyCurrent = 0;
-        public double driveMotorStatorCurrent = 0;
-        public double driveMotorSupplyCurrentOverTime = 0;
-        public double driveMotorStatorCurrentOverTime = 0;
-        public double driveMotorPosition = 0;
-        public double driveMotorAppliedVoltage = 0;
-
-        public double angle = 0;
-        public double angleSetpoint = 0;
-        public double absolutePosition = 0;
-        public double angleMotorVelocity = 0;
-        public double angleMotorSupplyCurrent = 0;
-        public double angleMotorStatorCurrent = 0;
-        public double angleMotorSupplyCurrentOverTime = 0;
-        public double angleMotorStatorCurrentOverTime = 0;
-        public double angleMotorPosition = 0;
-        public double angleMotorAppliedVoltage = 0;
-
-        public double moduleDistance = 0;
-    }
 }
