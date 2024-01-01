0b5b7c28fac9074223ea73fe0e8229bdef491237
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-01-01 10:04:00 +0200

    Create Vision.java

---
+++ src/main/java/frc/robot/vision/Vision.java
@@ -0,0 +1,51 @@
+package frc.robot.vision;
+
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.swerve.SwerveDrive;
+import org.littletonrobotics.junction.Logger;
+
+import static frc.robot.vision.VisionConstants.ROBOT_TO_CAM;
+
+public class Vision extends SubsystemBase {
+
+    private static Vision INSTANCE = null;
+
+    private final VisionModule[] modules;
+    private final Result[] results;
+
+    private Vision(VisionModule... modules) {
+        this.modules = modules;
+        results = new Result[modules.length];
+    }
+
+    public static Vision getInstance(boolean isReal, VisionModule[] modules) {
+        if (INSTANCE == null) {
+            if (isReal) {
+                INSTANCE = new Vision(
+                    modules
+                );
+            } else {
+                INSTANCE = new Vision(
+                        VisionModule.simIO("simCam", 0)
+                );
+            }
+        }
+        return INSTANCE;
+    }
+
+    public Result[] getResults() {
+        return results;
+    }
+
+    @Override
+    public void periodic() {
+        for (int i = 0; i < modules.length; i++) {
+            VisionModule module = modules[i];
+            module.io.updateInputs(module.inputs);
+            Logger.getInstance().processInputs(module.name, module.inputs);
+            results[i] = module.io.getLatestResult();
+            Logger.getInstance().recordOutput("RobotToCamLeft", new Pose3d(SwerveDrive.getInstance().getBotPose()).plus(ROBOT_TO_CAM[0]));
+        }
+    }
+}
