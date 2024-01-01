53768e2f9acb3fa017b5ab6bf9c2f148b496876c
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-01-01 10:04:07 +0200

    Create VisionConstants.java

---
+++ src/main/java/frc/robot/vision/VisionConstants.java
@@ -0,0 +1,23 @@
+package frc.robot.vision;
+
+import edu.wpi.first.math.geometry.*;
+import org.photonvision.SimVisionTarget;
+
+public class VisionConstants {
+    public static final Transform3d[] ROBOT_TO_CAM = new Transform3d[]{
+            new Transform3d( //left cam
+                    new Translation3d(-0.249, 0.30397, 0.5023),
+                    new Rotation3d(0, 0, Math.toRadians(142.5))),
+            new Transform3d( //right cam
+                    new Translation3d(-0.2363, -0.527, 0.196),
+                    new Rotation3d(0, 28, Math.toRadians(-151)))};
+
+    public static final Transform3d LIME_OFFSET = new Transform3d(new Translation3d( (16.54) / 2,(8.02) / 2, 0), new Rotation3d());
+    public static final double TARGET_WIDTH = 0.27;
+    public static final double TARGET_LENGTH = 0.27;
+    public static final Pose3d[] TARGET_POSITION_REAL = new Pose3d[]{new Pose3d() ,new Pose3d(new Translation3d( 7.24310, -2.93659, 0.46272), new Rotation3d(0,0,180)),new Pose3d(new Translation3d(7.24310, 1.26019, 0.46272), new Rotation3d(0,0,180)), new Pose3d(new Translation3d(7.24310, 0.41621, 0.41621), new Rotation3d(0,0,180)), new Pose3d(new Translation3d(7.90832, 2.74161, 0.695452), new Rotation3d(0,0,180)), new Pose3d(new Translation3d(-7.90832, 2.74161, 0.695452), new Rotation3d()), new Pose3d(new Translation3d(-7.24310, 0.41621, 0.46272), new Rotation3d()), new Pose3d(new Translation3d(-7.24310, 1.26019, 0.46272), new Rotation3d()), new Pose3d(new Translation3d(-7.24310, -2.93659, 0.46272), new Rotation3d())};
+    public static final Pose3d[] TARGET_POSITION_LIME = new Pose3d[]{new Pose3d() ,new Pose3d(new Translation3d( 7.24310, -2.93659, 0.46272), new Rotation3d(0,0,180)),new Pose3d(new Translation3d(7.24310, 1.26019, 0.46272), new Rotation3d(0,0,180)), new Pose3d(new Translation3d(7.24310, 0.41621, 0.41621), new Rotation3d(0,0,180)), new Pose3d(new Translation3d(7.90832, 2.74161, 0.695452), new Rotation3d(0,0,180)), new Pose3d(new Translation3d(-7.90832, 2.74161, 0.695452), new Rotation3d()), new Pose3d(new Translation3d(-7.24310, 0.41621, 0.46272), new Rotation3d()), new Pose3d(new Translation3d(-7.24310, 1.26019, 0.46272), new Rotation3d()), new Pose3d(new Translation3d(-7.24310, -2.93659, 0.46272), new Rotation3d())};
+    public static final Pose3d[] TARGET_POSITION_SIM = new Pose3d[]{new Pose3d() ,new Pose3d(new Translation3d( 7.24310, -2.93659, 0.46272), new Rotation3d(0,0,180)),new Pose3d(new Translation3d(7.24310, 1.26019, 0.46272), new Rotation3d(0,0,180)), new Pose3d(new Translation3d(7.24310, 0.41621, 0.41621), new Rotation3d(0,0,180)), new Pose3d(new Translation3d(7.90832, 2.74161, 0.695452), new Rotation3d(0,0,180)), new Pose3d(new Translation3d(-7.90832, 2.74161, 0.695452), new Rotation3d()), new Pose3d(new Translation3d(-7.24310, 0.41621, 0.46272), new Rotation3d()), new Pose3d(new Translation3d(-7.24310, 1.26019, 0.46272), new Rotation3d()), new Pose3d(new Translation3d(-7.24310, -2.93659, 0.46272), new Rotation3d())};
+
+    public static final SimVisionTarget[] VISION_TARGETS_SIM = new SimVisionTarget[]{};
+}
