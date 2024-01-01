6abc888a485376bf55f25c919f2ab9284046236d
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-01-01 10:08:25 +0200

    Removed unsused imports

--- src/main/java/frc/robot/vision/VisionIO.java
+++ src/main/java/frc/robot/vision/VisionIO.java
@@ -1,9 +1,5 @@
 package frc.robot.vision;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Transform3d;
-import frc.robot.Robot;
 import org.littletonrobotics.junction.AutoLog;
 
 public interface VisionIO {
