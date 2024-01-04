5735c7071629d24e675523201353efcf3490d07e
vichik123 <eitan.vichik@gmail.com>
2024-01-04 18:25:06 +0200

    Remove unused joysticks

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,17 +13,7 @@ public class RobotContainer {
     private static RobotContainer INSTANCE = null;
     private final SwerveDrive swerveDrive = SwerveDrive.getInstance();
     private final XboxController xboxController = new XboxController(0);
-    private final Joystick leftJoystick = new Joystick(1);
-    private final Joystick rightJoystick = new Joystick(2);
     private final JoystickButton a = new JoystickButton(xboxController, XboxController.Button.kA.value);
-    private final JoystickButton b = new JoystickButton(xboxController, XboxController.Button.kB.value);
-    private final JoystickButton y = new JoystickButton(xboxController, XboxController.Button.kY.value);
-    private final JoystickButton x = new JoystickButton(xboxController, XboxController.Button.kX.value);
-    private final JoystickButton back = new JoystickButton(xboxController, XboxController.Button.kBack.value);
-    private final JoystickButton rb = new JoystickButton(xboxController, XboxController.Button.kRightBumper.value);
-    private final JoystickButton lb = new JoystickButton(xboxController, XboxController.Button.kLeftBumper.value);
-    private final Trigger xboxRightTrigger = new Trigger(() -> xboxController.getRightTriggerAxis() > 0.2);
-    private final Trigger xboxLeftTrigger = new Trigger(() -> xboxController.getLeftTriggerAxis() > 0.2);
 
     /**
      * The container for the robot.  Contains subsystems, OI devices, and commands.
