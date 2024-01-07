7c2b3ccf79b0afcaa9fb9b3f8a2f06b9d4dbce62
Emma <emmalivne.03@gmail.com>
2024-01-07 09:21:56 +0200

    Make the checkModule default option return Commands.none instead of null

--- src/main/java/frc/robot/swerve/ModuleIO.java
+++ src/main/java/frc/robot/swerve/ModuleIO.java
@@ -4,6 +4,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 
 public interface ModuleIO {
     void updateInputs(SwerveModuleInputs inputs);
@@ -30,6 +31,6 @@ public interface ModuleIO {
     }
 
     default Command checkModule() {
-        return null;
+        return Commands.none();
     }
 }
