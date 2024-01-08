bbc196eddc45ec61203673abe7826f9935d14fa2
vichik123 <eitan.vichik@gmail.com>
2024-01-08 21:45:38 +0200

    R Remove inputs from VisionModule

--- src/main/java/frc/robot/vision/VisionModule.java
+++ src/main/java/frc/robot/vision/VisionModule.java
@@ -7,26 +7,22 @@ public class VisionModule {
 
     public final String name;
     public final VisionIO io;
-    public final VisionInputsAutoLogged inputs;
 
-    public VisionModule(String name, VisionIO io, VisionInputsAutoLogged inputs) {
+    public VisionModule(String name, VisionIO io) {
         this.name = name;
         this.io = io;
-        this.inputs = inputs;
     }
 
     public static VisionModule photonVisionIO(String name, int index) {
         return new VisionModule(
                 name,
-                new PhotonVisionIO(new PhotonCamera(name), VisionConstants.ROBOT_TO_CAM[index]),
-                new VisionInputsAutoLogged()
+                new PhotonVisionIO(new PhotonCamera(name), VisionConstants.ROBOT_TO_CAM[index])
         );
     }
     public static VisionModule simIO(String name,  int index){
         return new VisionModule(
                 name,
-                new VisionSimIO(VisionConstants.ROBOT_TO_CAM[index]),
-                new VisionInputsAutoLogged()
+                new VisionSimIO(VisionConstants.ROBOT_TO_CAM[index])
         );
     }
 }
