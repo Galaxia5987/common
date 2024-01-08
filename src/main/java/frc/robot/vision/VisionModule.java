7ed929f7b4a203f227182a03e6a64a77e46b33b5
vichik123 <eitan.vichik@gmail.com>
2024-01-08 21:57:37 +0200

    Add ROBOT_TO_CAM in VisionModule constructor

--- src/main/java/frc/robot/vision/VisionModule.java
+++ src/main/java/frc/robot/vision/VisionModule.java
@@ -7,10 +7,14 @@ public class VisionModule {
 
     public final String name;
     public final VisionIO io;
+    public final VisionInputsAutoLogged inputs;
+    public final Transform3d ROBOT_TO_CAM;
 
-    public VisionModule(String name, VisionIO io) {
+    public VisionModule(String name, VisionIO io, Transform3d ROBOT_TO_CAM) {
         this.name = name;
         this.io = io;
+        this.inputs = new VisionInputsAutoLogged();
+        this.ROBOT_TO_CAM = ROBOT_TO_CAM;
     }
 
     public static VisionModule photonVisionIO(String name, int index) {
