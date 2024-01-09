ba6e84a5c0ee837e07aa63878cb80304d6de353d
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-01-09 19:24:38 +0200

    change robotToCam to get value from io

--- src/main/java/frc/robot/vision/VisionModule.java
+++ src/main/java/frc/robot/vision/VisionModule.java
@@ -1,18 +1,15 @@
 package frc.robot.vision;
 
 import edu.wpi.first.math.geometry.Transform3d;
-import org.photonvision.PhotonCamera;
 
 public class VisionModule {
-
-    public final String name;
     public final VisionIO io;
     public final VisionInputsAutoLogged inputs;
-    public final Transform3d ROBOT_TO_CAM;
+    public final Transform3d robotToCam;
 
     public VisionModule(VisionIO io) {
         this.io = io;
         this.inputs = new VisionInputsAutoLogged();
-        this.ROBOT_TO_CAM = ROBOT_TO_CAM;
+        this.robotToCam = io.getCameraToRobot();
     }
 }
