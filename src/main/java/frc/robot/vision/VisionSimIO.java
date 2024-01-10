6bb29e876f27b11fcd7b015543b580281fbe44f6
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-01-10 11:09:08 +0200

    Optimized updateInputs

--- src/main/java/frc/robot/vision/VisionSimIO.java
+++ src/main/java/frc/robot/vision/VisionSimIO.java
@@ -8,9 +8,9 @@ import lib.Utils;
 import org.photonvision.PhotonCamera;
 import org.photonvision.estimation.TargetModel;
 import org.photonvision.simulation.PhotonCameraSim;
-import org.photonvision.simulation.VisionSystemSim;
 import org.photonvision.simulation.VisionTargetSim;
 import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
 
 public class VisionSimIO implements VisionIO {
     private SimVisionSystem simVisionSystem;
@@ -59,12 +59,13 @@ public class VisionSimIO implements VisionIO {
     @Override
     public void updateInputs(VisionInputs inputs) {
         var pose = SwerveDrive.getInstance().getBotPose();
+        var pose3d = Utils.pose2dToPose3d(pose);
 
-        simVisionSystem.update(Utils.pose2dToPose3d(pose));
+        simVisionSystem.update();
         latestResult =
                 cameraSim.process(
                         0,
-                        Utils.pose2dToPose3d(pose).plus(robotToCam.div(-1)),
+                        pose3d.plus(robotToCam.div(-1)),
                         tagFieldLayout.getTags().stream()
                                 .map(
                                         (a) ->
@@ -73,27 +74,27 @@ public class VisionSimIO implements VisionIO {
                                 .toList());
         inputs.latency = (long) latestResult.getLatencyMillis();
         inputs.hasTargets = latestResult.hasTargets();
+        if (inputs.hasTargets) {
+            PhotonTrackedTarget bestTarget = latestResult.getBestTarget();
+            if (bestTarget != null) {
+                inputs.area = bestTarget.getArea();
+                inputs.pitch = bestTarget.getPitch();
+                inputs.yaw = bestTarget.getYaw();
+                inputs.targetSkew = bestTarget.getSkew();
+                inputs.targetID = bestTarget.getFiducialId();
+                inputs.targetAmbiguity = bestTarget.getPoseAmbiguity();
 
-        if (latestResult.getBestTarget() != null) {
-            inputs.area = latestResult.getBestTarget().getArea();
-            inputs.pitch = latestResult.getBestTarget().getPitch();
-            inputs.yaw = latestResult.getBestTarget().getYaw();
-            inputs.targetSkew = latestResult.getBestTarget().getSkew();
-            inputs.targetID = latestResult.getBestTarget().getFiducialId();
-            inputs.targetAmbiguity = latestResult.getBestTarget().getPoseAmbiguity();
-
-            var cameraToTarget = latestResult.getBestTarget().getBestCameraToTarget();
-            inputs.cameraToTarget =
-                    new double[] {
-                        cameraToTarget.getX(),
-                        cameraToTarget.getY(),
-                        cameraToTarget.getZ(),
-                        cameraToTarget.getRotation().getX(),
-                        cameraToTarget.getRotation().getY(),
-                        cameraToTarget.getRotation().getZ()
-                    };
-        } else {
-            result = null;
+                var cameraToTarget = bestTarget.getBestCameraToTarget();
+                inputs.cameraToTarget =
+                        new double[] {
+                            cameraToTarget.getX(),
+                            cameraToTarget.getY(),
+                            cameraToTarget.getZ(),
+                            cameraToTarget.getRotation().getX(),
+                            cameraToTarget.getRotation().getY(),
+                            cameraToTarget.getRotation().getZ()
+                        };
+            }
         }
     }
 }
