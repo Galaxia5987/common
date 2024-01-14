83b731ae55ac76b2037eb6a269b1b242984e2bda
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-01-14 22:44:19 +0200

    Add HeatMapConstants.java

---
+++ src/main/java/frc/robot/vision/heatMap/HeatMapConstants.java
@@ -0,0 +1,9 @@
+package frc.robot.vision.heatMap;
+
+public class HeatMapConstants {
+    public static int heightMaximum = 80; // cm
+    public static int heightMinimumRange = 20; // cm
+    public static int heightJumps = 5; // cm
+    public static int robotAngleJumps = 5;
+    public static double iterationTime = 0.04; // s
+}
