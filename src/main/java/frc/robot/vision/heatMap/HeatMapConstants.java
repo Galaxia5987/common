fbc5f3409074a110ccfa50288abc38dbaa16aaf4
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-01-17 20:24:27 +0200

    Change heightMaximum units to meters

--- src/main/java/frc/robot/vision/heatMap/HeatMapConstants.java
+++ src/main/java/frc/robot/vision/heatMap/HeatMapConstants.java
@@ -5,5 +5,6 @@ public class HeatMapConstants {
     public static int heightMinimumRange = 20; // cm
     public static int heightJumps = 5; // cm
     public static int robotAngleJumps = 5;
+    public static double heightMaximum = 0.8; // m
     public static double iterationTime = 0.04; // s
 }
