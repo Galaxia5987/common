e8ae8b416db9aaf7509ca3a86765f444ab0f10bf
vichik123 <eitan.vichik@gmail.com>
2024-01-17 22:35:03 +0200

    Pass field layout through initialize in SimVisionSystem

--- src/main/java/frc/robot/vision/SimVisionSystem.java
+++ src/main/java/frc/robot/vision/SimVisionSystem.java
@@ -1,7 +1,6 @@
 package frc.robot.vision;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
-import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Transform3d;
 import frc.robot.swerve.SwerveDrive;
 import org.photonvision.simulation.PhotonCameraSim;
@@ -12,11 +11,9 @@ public class SimVisionSystem {
     private static final VisionSystemSim visionSim = new VisionSystemSim("main");
     private AprilTagFieldLayout tagFieldLayout;
 
-    private SimVisionSystem() {
+    private SimVisionSystem(AprilTagFieldLayout fieldLayout) {
         try {
-            tagFieldLayout =
-                    AprilTagFieldLayout.loadFromResource(
-                            AprilTagFields.k2024Crescendo.m_resourceFile);
+            tagFieldLayout = fieldLayout;
         } catch (Exception e) {
             return;
         }
@@ -24,18 +21,16 @@ public class SimVisionSystem {
     }
 
     public static SimVisionSystem getInstance(PhotonCameraSim cameraSim, Transform3d robotToCam) {
-        if (INSTANCE == null) {
-            INSTANCE = new SimVisionSystem();
-        }
         visionSim.addCamera(cameraSim, robotToCam);
         return INSTANCE;
     }
 
     public static SimVisionSystem getInstance() {
-        if (INSTANCE == null) {
-            INSTANCE = new SimVisionSystem();
-        }
         return INSTANCE;
+    }
+
+    public static void initialize(AprilTagFieldLayout fieldLayout) {
+        INSTANCE = new SimVisionSystem(fieldLayout);
     }
 
     public void update() {
