cc459dde59a5e03f5134bf61414aa2dc1bfdb437
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-01-17 20:24:58 +0200

    Change constants units to meters

--- src/main/java/frc/robot/vision/heatMap/HeatMapConstants.java
+++ src/main/java/frc/robot/vision/heatMap/HeatMapConstants.java
@@ -6,5 +6,7 @@ public class HeatMapConstants {
     public static int heightJumps = 5; // cm
     public static int robotAngleJumps = 5;
     public static double heightMaximum = 0.8; // m
+    public static double heightMinimumRange = 0.2; // m
+    public static double heightJumps = 0.05; // m
     public static double iterationTime = 0.04; // s
 }
