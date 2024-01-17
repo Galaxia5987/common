2f0c312cbd3a92e42a443be5dedd4655283c0cb5
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-01-17 20:25:52 +0200

    Add field size constants

--- src/main/java/frc/robot/vision/heatMap/HeatMapConstants.java
+++ src/main/java/frc/robot/vision/heatMap/HeatMapConstants.java
@@ -1,13 +1,13 @@
 package frc.robot.vision.heatMap;
 
 public class HeatMapConstants {
-    public static int heightMaximum = 80; // cm
-    public static int heightMinimumRange = 20; // cm
-    public static int heightJumps = 5; // cm
-    public static int robotAngleJumps = 5;
     public static double heightMaximum = 0.8; // m
     public static double heightMinimumRange = 0.2; // m
     public static double heightJumps = 0.05; // m
     public static double robotAngleJumps = 5; //deg
     public static double iterationTime = 0.04; // s
+
+    public static final double squareLength = 0.5; // m
+    public static final double yLength = 8; // m
+    public static final double xLength = 4; // m
 }
