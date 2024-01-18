b01e68e50c107d65f9cb1699f78d5634e7c417fa
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-01-18 22:22:09 +0200

    Multiplied df to see the values

--- src/main/java/frc/robot/vision/heatMap/visualizeHeatMap.py
+++ src/main/java/frc/robot/vision/heatMap/visualizeHeatMap.py
@@ -5,10 +5,8 @@ import numpy as np
 
 def plot_heatmap(csv_file, multiplier=1000000, showValues=False):
     df = pd.read_csv(csv_file, index_col=0)
+    df *= multiplier  # Needed if you want to see the values in each grid
 
-    plt.figure(figsize=(10, 8))
-    sns.heatmap(df, cmap="YlGnBu", annot=True, fmt=".2f", linewidths=.5)
-    plt.title('Vision HeatMap')
     unique_values = np.unique(df.values.flatten())
     sorted_values = np.sort(unique_values)[::-1]
     second_max_value = sorted_values[1] if len(sorted_values) > 1 else np.nan
