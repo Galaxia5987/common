9bf95a1fca159ea488b19fd08e29a87f997127e9
vichik123 <eitan.vichik@gmail.com>
2024-01-19 20:05:06 +0200

    Remove unnecessary util functions

--- src/main/java/tests/TestUtils.java
+++ src/main/java/tests/TestUtils.java
@@ -1,9 +1,6 @@
 package tests;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
 import lib.Utils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -33,56 +30,6 @@ public class TestUtils {
         Assertions.assertTrue(Utils.epsilonEquals(-1, -1.5, 0.5));
         Assertions.assertTrue(Utils.epsilonEquals(1.5, 1, 0.5));
         Assertions.assertTrue(Utils.epsilonEquals(-1.5, -1, 0.5));
-    }
-
-    @Test
-    public void testPose2dConversions() {
-        double x = 1, y = 1, rot = 1;
-        Pose2d poseObject = new Pose2d(1, 1, new Rotation2d(1));
-        double[] poseArray = new double[] {x, y, rot};
-
-        Assertions.assertArrayEquals(poseArray, Utils.pose2dToArray(poseObject), Utils.EPSILON);
-        Assertions.assertEquals(poseObject.toString(), Utils.arrayToPose2d(poseArray).toString());
-    }
-
-    @Test
-    public void testChassisSpeedsConversions() {
-        double x = 1, y = 1, omega = 1;
-        double[] chassisSpeedsArray = new double[] {x, y, omega};
-        ChassisSpeeds chassisSpeedsObject = new ChassisSpeeds(x, y, omega);
-
-        Assertions.assertArrayEquals(
-                chassisSpeedsArray, Utils.chassisSpeedsToArray(chassisSpeedsObject), Utils.EPSILON);
-        Assertions.assertEquals(
-                Utils.arrayToChassisSpeeds(chassisSpeedsArray).toString(),
-                chassisSpeedsObject.toString());
-    }
-
-    @Test
-    public void testSwerveModuleConversions() {
-        double vel1 = 1, angle1 = 1, vel2 = 2, angle2 = 0.5;
-        double[] swerveModuleStatesArray = new double[] {angle1, vel1, angle2, vel2};
-        SwerveModuleState[] swerveModuleStates =
-                new SwerveModuleState[] {
-                    new SwerveModuleState(vel1, new Rotation2d(angle1)),
-                    new SwerveModuleState(vel2, new Rotation2d(angle2))
-                };
-
-        SwerveModuleState[] convertedSwerveModuleStates =
-                Utils.arrayToSwerveModuleStates(swerveModuleStatesArray);
-
-        Assertions.assertEquals(swerveModuleStates.length, convertedSwerveModuleStates.length);
-        for (int i = 0; i < convertedSwerveModuleStates.length; i++) {
-            Assertions.assertEquals(
-                    "Module " + (i + 1),
-                    swerveModuleStates[i].toString(),
-                    convertedSwerveModuleStates[i].toString());
-        }
-
-        Assertions.assertArrayEquals(
-                swerveModuleStatesArray,
-                Utils.swerveModuleStatesToArray(swerveModuleStates),
-                Utils.EPSILON);
     }
 
     @Test
