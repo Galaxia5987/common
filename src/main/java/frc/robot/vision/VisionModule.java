eecb2bcc036d8670516491e0d412008a13ad6a3c
vichik123 <eitan.vichik@gmail.com>
2024-01-24 17:23:56 +0200

    Fix support of multiple cameras per module

--- src/main/java/frc/robot/vision/VisionModule.java
+++ src/main/java/frc/robot/vision/VisionModule.java
@@ -2,19 +2,26 @@ package frc.robot.vision;
 
 import edu.wpi.first.math.geometry.Transform3d;
 import java.util.Arrays;
+import lib.Utils;
 
 public class VisionModule {
     public final VisionIO[] ios;
-    public final VisionInputsAutoLogged inputs;
+    public final VisionInputsAutoLogged[] inputs;
     public final Transform3d[] robotToCams;
 
     public VisionModule(VisionIO... ios) {
         this.ios = ios;
-        this.inputs = new VisionInputsAutoLogged();
+        this.inputs = new VisionInputsAutoLogged[ios.length];
+        Arrays.setAll(inputs, (i) -> new VisionInputsAutoLogged());
         robotToCams =
                 Arrays.stream(ios)
                         .map(VisionIO::getCameraToRobot)
                         .toList()
                         .toArray(new Transform3d[0]);
+    }
+
+    public double getAverageAmbiguity() {
+        return Utils.averageAmbiguity(
+                Arrays.stream(inputs).map((inputs) -> inputs.averageAmbiguity).toList());
     }
 }
