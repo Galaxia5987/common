126c83e5781e9aae309abd67b947f24bb73aca18
Emma <emmalivne.03@gmail.com>
2024-02-02 11:51:23 +0200

    Separate logic and changing state

--- src/main/java/frc/robot/leds/LedState.java
+++ src/main/java/frc/robot/leds/LedState.java
@@ -8,6 +8,9 @@ public class LedState {
     private Color secondary;
     private double blinkTime;
     private double fadeDuration;
+    private int percentage;
+    private int startingLed;
+    private int endingLed;
 
     public LedState(LedMode mode, Color primary, Color secondary, double blinkTime, double fadeDuration) {
         this.mode = mode;
@@ -15,6 +18,33 @@ public class LedState {
         this.secondary = secondary;
         this.blinkTime = blinkTime;
         this.fadeDuration = fadeDuration;
+    }
+
+    public LedState(){
+        this.mode = LedMode.SOLID;
+        this.primary = Color.kBlack;
+        this.secondary = Color.kBlack;
+        this.blinkTime = 1;
+        this.fadeDuration = 1;
+        this.percentage = 100; //TODO: check if needs to be 100
+        this.startingLed = 1;
+        this.endingLed = 1;
+    }
+
+    /**
+     * @return Active length of the strip.
+     */
+    public int getStripLength(){
+        return endingLed-startingLed+1;
+    }
+
+    public void setStripLength(int length){
+        setEndingLed(startingLed-1+length);
+    }
+
+    public void setStripLength(int startingLed, int endingLed){
+        setStartingLed(startingLed);
+        setEndingLed(endingLed);
     }
 
     public LedMode getMode() {
@@ -29,6 +59,12 @@ public class LedState {
         return primary;
     }
 
+    /**
+     * Sets the primary color of the LEDs that will be used
+     * in the solid and percentage modes and as the first color
+     * in the blink and fade modes.
+     * @param primary Color to set the primary.
+     */
     public void setPrimary(Color primary) {
         this.primary = primary;
     }
@@ -37,6 +73,11 @@ public class LedState {
         return secondary;
     }
 
+    /**
+     * Sets the secondary color of the LEDs that will be used
+     * as the second color in the blink and fade modes.
+     * @param secondary Color to set the secondary.
+     */
     public void setSecondary(Color secondary) {
         this.secondary = secondary;
     }
@@ -45,6 +86,10 @@ public class LedState {
         return blinkTime;
     }
 
+    /**
+     * Sets the blink time of the LEDs.
+     * @param time Time it takes to change the color. [sec]
+     */
     public void setBlinkTime(double time) {
         this.blinkTime = time;
     }
@@ -53,7 +98,39 @@ public class LedState {
         return fadeDuration;
     }
 
+    /**
+     * Sets the duration for the fade effect.
+     * @param duration Duration of the fade effect. [sec]
+     */
     public void setFadeDuration(double duration) {
         this.fadeDuration = duration;
+    }
+
+    public int getPercentage() {
+        return percentage;
+    }
+
+    /**
+     * Sets the percentage of the active strip length to use.
+     * @param percentage Percent of active strip length. [%]
+     */
+    public void setPercentage(int percentage) {
+        this.percentage = percentage;
+    }
+
+    public int getStartingLed() {
+        return startingLed;
+    }
+
+    public void setStartingLed(int startingLed) {
+        this.startingLed = startingLed;
+    }
+
+    public int getEndingLed() {
+        return endingLed;
+    }
+
+    public void setEndingLed(int endingLed) {
+        this.endingLed = endingLed;
     }
 }
--- src/main/java/frc/robot/leds/LedStrip.java
+++ src/main/java/frc/robot/leds/LedStrip.java
@@ -10,25 +10,16 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class LedStrip extends SubsystemBase {
     private final AddressableLED ledStrip;
     private AddressableLEDBuffer ledBuffer;
-    private int stripLength = 0;
-    private int offset = 0;
-    private int percentage = 100;
+    private LedState state = new LedState();
 
-    private double blinkTime = 1;
-    private double fadeDuration = 1;
-
-    private Color primary = new Color();
-    private Color secondary = new Color();
-    private Color currentColor = primary;
-    private Color fadeColor = primary;
+    private Color currentColor = state.getPrimary();
+    private Color fadeColor = state.getPrimary();
 
     private final Timer timer = new Timer();
-    private LedMode mode = LedMode.SOLID;
 
     public LedStrip(int port, int length) {
         ledStrip = new AddressableLED(port);
         ledBuffer = new AddressableLEDBuffer(length);
-        stripLength = length;
 
         ledStrip.setLength(length);
         ledStrip.setData(ledBuffer);
@@ -38,76 +29,13 @@ public class LedStrip extends SubsystemBase {
         timer.reset();
     }
 
-    public void setLength(int length) {
-        ledBuffer = new AddressableLEDBuffer(length);
-        ledStrip.setLength(length);
-        stripLength = length;
-    }
-
     public void setState(LedState state){
-        setBlinkTime(state.getBlinkTime());
-        setFadeDuration(state.getFadeDuration());
-        setMode(state.getMode());
-        setPrimary(state.getPrimary());
-        setSecondary(state.getSecondary());
-    }
-
-    /**
-     * Sets the primary color of the LEDs.
-     * It will be used when in the solid mode and as the
-     * initial color in the blink and fade modes
-     * @param color
-     */
-    private void setPrimary(Color color) {
-        primary = color;
-    }
-
-    /**
-     * Sets the secondary color of the LEDs.
-     * It will be used as the second color in the blink and fade modes
-     * @param color
-     */
-    private void setSecondary(Color color) {
-        secondary = color;
-    }
-
-    private void setMode(LedMode mode) {
-        this.mode = mode;
-    }
-
-    /**
-     * Sets the offset of the LEDs.
-     * @param offset The amount of LEDs that won't light.
-     *               Counts from the start of the strip.
-     */
-    public void setOffset(int offset){
-        setSolidColor(Color.kBlack, 0, offset+1);
-        this.offset = offset;
-    }
-
-    public void setPercentage(int percentage) {
-        this.percentage = percentage;
-    }
-
-    /**
-     * Sets the led blink time.
-     * @param blinkTime Blink time in seconds
-     */
-    private void setBlinkTime(double blinkTime) {
-        this.blinkTime = blinkTime;
-    }
-
-    /**
-     * Sets the duration of the fade effect.
-     * @param duration Fade duration in seconds.
-     */
-    private void setFadeDuration(double duration) {
-        fadeDuration = duration;
+        this.state = state;
     }
 
     private void setSolidColor(Color color) {
         var appliedColor = new Color(color.red, color.green, color.blue);
-        for (int i = offset; i < stripLength; i++) {
+        for (int i = state.getStartingLed()-1; i < state.getEndingLed(); i++) {
             ledBuffer.setLED(i, appliedColor);
         }
         ledStrip.setData(ledBuffer);
@@ -131,7 +59,7 @@ public class LedStrip extends SubsystemBase {
         Translation3d initialPoint = new Translation3d(initial.red, initial.green, initial.blue);
         Translation3d goalPoint = new Translation3d(goal.red, goal.green, goal.blue);
 
-        double d = initialPoint.getDistance(goalPoint) / fadeDuration;
+        double d = initialPoint.getDistance(goalPoint) / state.getFadeDuration();
         double t = d / (initialPoint.minus(goalPoint)).getNorm();
 
         Translation3d solution = initialPoint.interpolate(goalPoint, t);
@@ -144,9 +72,9 @@ public class LedStrip extends SubsystemBase {
 
     private void setRainbow() {
         int rainbowHue = 0;
-        for (int i = offset; i < stripLength; i++) {
+        for (int i = state.getStartingLed()-1; i < state.getEndingLed(); i++) {
             ledBuffer.setHSV(i, rainbowHue, 255, 180);
-            rainbowHue += (180 / stripLength);
+            rainbowHue += (180 / state.getStripLength());
             ledStrip.setData(ledBuffer);
             rainbowHue %= 180;
         }
@@ -154,24 +82,27 @@ public class LedStrip extends SubsystemBase {
 
     @Override
     public void periodic() {
-        switch (mode){
+        switch (state.getMode()){
             case SOLID:
-                setSolidColor(primary);
+                setSolidColor(state.getPrimary());
 
             case PERCENTAGE:
-                setSolidColor(primary, 0, stripLength*percentage/100);
+                setSolidColor(
+                        state.getPrimary(),
+                        0,
+                        state.getStripLength()*state.getPercentage()/100);
 
             case BLINK:
-                if (currentColor == primary) currentColor = secondary;
-                else currentColor = primary;
+                if (currentColor == state.getPrimary()) currentColor = state.getSecondary();
+                else currentColor = state.getPrimary();
 
-                if (timer.advanceIfElapsed(blinkTime)) {
+                if (timer.advanceIfElapsed(state.getBlinkTime())) {
                     setSolidColor(currentColor);
                 }
                 timer.reset();
 
             case FADE:
-                updateFade(primary, secondary);
+                updateFade(state.getPrimary(), state.getSecondary());
                 setSolidColor(fadeColor);
 
             case RAINBOW:
